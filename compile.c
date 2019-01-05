ef67b1996dec6789450d385d051d2deb102f4494
BenjaminArbe <fernandoarbe@bol.com.br>
2019-01-04 22:47:44 -0300

    Added class character functionality with related tests

--- compile.c
+++ compile.c
@@ -15,6 +15,7 @@ int ncbra;
 int reglength, regerrno;
 
 static char *ep;
+static char *lastep;
 static char *sp;
 static char cbracket[NCBRA];
 static char *cbracketp;
@@ -40,18 +41,12 @@ compile ( const char *instring, char *expbuf, char *endbuf) {
 	int c;
 	sp = (char *)instring;
 	ep = expbuf;
-	char *lastep = 0;
+	lastep = 0;
 	ncbra = 0;
 	cbracketp = &cbracket[0];
 	regerrno = reglength = 0;
 
-	if ( *sp == '\0' ) {
-		if ( inUseMalloc && expbuf )
-			free (expbuf);
-		regerrno = 41;
-		reglength = 0;
-		goto error;
-	}
+	if ( *sp == '\0' ) { regerrno = 41; goto error; }
 	
 	if ( *sp == '^' ) { bcf = true; sp++; }
 	
@@ -61,15 +56,11 @@ compile ( const char *instring, char *expbuf, char *endbuf) {
 			// Before we quit, check cbracket balance
 			// This block also takes care of the left unbalance
 			if ( cbracketp != cbracket ) {
-				regerrno = 42;
-				reglength = 0;
-				return 0;
-			}
+				regerrno = 42; goto error; }
 			// Now the setup to quit
 			*ep++ = CEOF;
 			*ep = 0;
 			reglength = ep - expbuf;
-			//return (inUseMalloc ? expbuf : ep);
 			if ( inUseMalloc )
 				return expbuf;
 			else
@@ -77,13 +68,7 @@ compile ( const char *instring, char *expbuf, char *endbuf) {
 		}
 		
 		// Buffer overflow check:
-		if ( ep >= endbuf ) {
-			reglength = 0;
-			regerrno = 50;
-			if ( inUseMalloc && expbuf ) 
-				free (expbuf);
-			goto error;
-		}
+		if ( ep >= endbuf ) { regerrno = 50; goto error; }
 		
 		// Remember ep if not a RE *
 		if ( c != '*' ) lastep = ep;
@@ -120,11 +105,7 @@ compile ( const char *instring, char *expbuf, char *endbuf) {
 			case '[': {
 				int ret = compile_char_class(endbuf);
 				if ( ret == 1 ) continue;
-				regerrno = 11; // Range endpoint too large
-				reglength = 0;
-				if ( inUseMalloc && expbuf ) 
-					free (expbuf);
-				goto error;
+				else if ( ret == -1 ) goto error;
 			}
 			
 defchar:
@@ -135,8 +116,11 @@ defchar:
 			
 	} 	// end of while
 	
-
+// Note: Before to get here, regerrno should be set.
 error:
+	reglength = 0;
+	if ( inUseMalloc && expbuf )
+		free(expbuf);
 	return 0;
 }
 
@@ -146,23 +130,21 @@ error:
  * returns:
  * 	1 means successfully compiled so ready to continue 
  *			compilation
- * 	0 fail to compile due a syntax error or buffer overrun
+ * 	-1 fail to compile due a syntax error or buffer overrun
  */
 static int
 compile_char_class(char *ebuf) {
 	*ep++ = CCL;
 	*ep++ = 0;	
-	int count = 1;
+	int count = 0;
 	char c = *sp++;
 	if ( c == '^' ) { c = *sp++; ep[-2] = NCCL; }
 	do {
 		if ( c == '\0' ) { // Missing ]
-			regerrno = 49; reglength = 0; return 0;
+			regerrno = 49; return -1;
 		}
-		
+
 		// If '-' is not the first char in the class:
-		// ep[-1] is 0 if ep is at the 1st char after [ or [^
-		// ep[-1] is the char before the '-' otherwise.
 		if ( c == '-' && ep[-1] != 0 ) {
 			// If ] comes right after '-' then consider '-'
 			// as normal char too;
@@ -173,11 +155,14 @@ compile_char_class(char *ebuf) {
 			}
 			// So we're dealing with a range:
 			// copy all chars in the range
-			while ( ep[-1] < c ) {
+			while ( ep[-1] < c - 1 ) {
 				*ep = ep[-1] + 1;
 				ep++; count++;
 				// Check for buffer overflow:
-				if ( ep >= ebuf ) return 0;
+				if ( ep >= ebuf ) {
+					regerrno = 11; // Range endpoint too large
+					return -1;
+				}
 			}
 		}
 		// Here is the normal char processing or 
@@ -185,9 +170,13 @@ compile_char_class(char *ebuf) {
 		*ep++ = c;
 		count++;
 		// Check for buffer overflow:
-		if ( ep >= ebuf ) return 0;
+		if ( ep >= ebuf ) {
+			regerrno = 11;	 // Range endpoint too large
+			return -1;
+		}
 		
 	} while ( (c = *sp++) != ']');
+	lastep[1] = count;
 	return 1;
 }
 
@@ -206,7 +195,6 @@ compile_groups(int c) {
 		case '(':
 			if ( ncbra >= NCBRA ) {
 				regerrno = 43;	// Too many \(
-				reglength = 0;
 				return -1;
 			}
 			*cbracketp++ = ncbra;
@@ -218,13 +206,17 @@ compile_groups(int c) {
 			// This block takes care of right unbalance
 			if ( cbracketp <= cbracket ) {
 				regerrno = 42;	// Unmatched \)
-				reglength = 0;
 				return -1;
 			}
 			*ep++ = CKET;
 			*ep++ = *--cbracketp;
 			return 1;
 		
+		case '0':
+			{ regerrno = 25; // \digit out of range
+				return -1;
+			}
+			
 		case '1': case '2' : case '3' : case '4' :
 		case '5': case '6': case '7': case '8': case '9':
 			*ep++ = CBACK;
---
+++ test/07compile_char_class_t.c
@@ -0,0 +1,110 @@
+#include <regexpr.h>
+#include <defs.h>
+#include "minunit.h"
+
+typedef struct {
+	char tok;
+	char ch;
+} cchar_t;
+
+char *
+compile_incomplete_cc() {
+	char *p = compile("abd[", 0, 0);
+	mu_assert(p == 0, "Failure compiling RE");
+	mu_assert(regerrno == 49, "Missing ]");
+	p = compile("abd[^", 0, 0);
+	mu_assert(p == 0, "Failure compiling RE");
+	mu_assert(regerrno == 49, "Missing ]");
+	
+	if (p) free(p);
+	return 0;
+}
+
+char *
+compile_normal_cc() {
+	char re[] = "abd[-*.abcjz]";
+	char *p = compile(re, 0, 0);
+	mu_assert(p != 0, "Failure compiling RE");
+	cchar_t *pc = p; int i;
+	for (i = 0; pc->tok == CCHR; pc++, i++) ;
+	char *ptr = pc;
+	mu_assert(*ptr++ == CCL, "Incorrect token found");
+	mu_assert(*ptr++ == 8, "Incorrect number of chars in class");
+	i++;
+	mu_assert(strncmp(ptr, &re[i], 8) == 0, "class char didn't match");
+	
+	if (p) free(p);
+	return 0;
+}
+
+char *
+compile_normal_neg_cc() {
+	char re[] = "abd[^-*.abcjz]";
+	char *p = compile(re, 0, 0);
+	mu_assert(p != 0, "Failure compiling RE");
+	cchar_t *pc = p; int i;
+	for (i = 0; pc->tok == CCHR; pc++, i++) ;
+	char *ptr = pc;
+	mu_assert(*ptr++ == NCCL, "Incorrect token found");
+	mu_assert(*ptr++ == 8, "Incorrect number of chars in class");
+	i++;i++;
+	mu_assert(strncmp(ptr, &re[i], 8) == 0, "class char didn't match");
+	
+	if (p) free(p);
+	return 0;
+}
+
+char *
+compile_normal_exception() {
+	char re[] = "[\a]";
+	char *p = compile(re, 0, 0);
+	mu_assert(p != 0, "Failure compiling RE");
+	mu_assert(p[0] == CCL, "Incorrect token found");
+	mu_assert(p[1] == 1, "Incorrect number of chars in class");
+	mu_assert(p[2] == '\a', "Incorrect char in class");
+	
+	if (p) free(p);
+	return 0;
+}
+
+char *
+compile_range_cc() {
+	char re[] = "[a-c]";
+	char *p = compile(re, 0, 0);
+	mu_assert(p != 0, "Failure compiling RE");
+	mu_assert(p[0] == CCL, "Incorrect token found");
+	mu_assert(p[1] == 3, "Incorrect number of chars in range");
+	mu_assert(strncmp(&p[2], "abc", 3)==0, "Incorrect range matched");
+	
+	if (p) free(p);
+	return 0;
+}
+
+char *
+compile_range_neg_cc() {
+	char re[] = "[^a-c]";
+	char *p = compile(re, 0, 0);
+	mu_assert(p != 0, "Failure compiling RE");
+	mu_assert(p[0] == NCCL, "Incorrect token found");
+	mu_assert(p[1] == 3, "Incorrect number of chars in range");
+	mu_assert(strncmp(&p[2], "abc", 3)==0, "Incorrect range matched");
+	
+	if (p) free(p);
+	return 0;
+}
+
+char *
+all_tests() {
+	mu_suite_start();
+	
+	mu_run_test(compile_incomplete_cc);
+	mu_run_test(compile_normal_cc);
+	mu_run_test(compile_normal_neg_cc);
+	mu_run_test(compile_normal_exception);
+	mu_run_test(compile_range_cc);
+	mu_run_test(compile_range_neg_cc);
+	
+	return 0;
+}
+
+RUN_TESTS(all_tests);
