b2086575ac8c21af367371906e02786716d37fbd
BenjaminArbe <fernandoarbe@bol.com.br>
2019-01-12 09:22:38 -0300

    Tests for rpt dot

---
+++ test/11step_dot_crpt_t.c
@@ -0,0 +1,36 @@
+#include <regexpr.h>
+#include "minunit.h"
+
+char *
+rpt_dot_one_parameter() {
+	char *p = compile("ab.\\{3\\}c",0, 0);
+	mu_assert(p != 0, "Failure compiling RE");
+	char s[] = "abcxabccyaabccczaabccccuababdc";
+	int r = step(s, p);
+	mu_assert(r == 1, "Failure matching RE");
+	mu_assert(strncmp(loc1, "abcccc", loc2-loc1) == 0,"wrong match");
+	mu_assert(*loc2 == 'u', "wrong loc2 value");
+	locs = loc2;
+	r = step(loc2, p);
+	mu_assert(r == 1, "Failure matching RE");
+	mu_assert(strncmp(loc1, "ababdc", loc2-loc1) == 0,"wrong match");
+	mu_assert(*loc2 == '\0', "wrong loc2 value");
+	locs = loc2;
+	r = step(loc2, p);
+	mu_assert(r == 0, "Failure matching RE");
+	
+	if (p) free(p);
+	return 0;
+}
+
+
+char *
+all_tests() {
+	mu_suite_start();
+	
+	mu_run_test(rpt_dot_one_parameter);
+	
+	return 0;
+}
+
+RUN_TESTS(all_tests);
